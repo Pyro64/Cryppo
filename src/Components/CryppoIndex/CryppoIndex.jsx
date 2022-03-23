a8a5b0fe37c207f05342834be4d1d7b1fa2f0330
kykakre@mail.ru <kykakre@mail.ru>
2022-03-23 16:18:31 +0400

    index

--- src/Components/CryppoIndex/CryppoIndex.jsx
+++ src/Components/CryppoIndex/CryppoIndex.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Description from '../Description/Description'
+import Subtitle from '../Subtitle/Subtitle'
+import Text from '../Text/Text'
+import Title from '../Title/Title'
 
 export default function CryppoIndex() {
     return (
-        <div>CryppoIndex</div>
+        <div className="container">
+        <Title text="CRYPPO INDEX" gradient="." />
+        <Text text="Автоматически ребалансируемый индекс из 8 самых высококапитализированных цифровых активов на основе машинного обучения (искусственного интеллекта)" />
+        
+        </div>
     )
 }
