d2eaebbce9d7ce8643526137c68a66331e5c0006
RomanCentr <trifonov.roman64@gmail.com>
2022-03-24 12:34:50 +0400

    12

--- src/Components/Subtitle/Subtitle.jsx
+++ src/Components/Subtitle/Subtitle.jsx
@@ -1,10 +1,21 @@
+
 import React from 'react'
 import style from './Subtitle.module.scss'
 export default function Subtitle(props) {
-    return (
-        <p className={style.subtitle}>
-            {props.subtitle}
-            <span>{props.gradient}</span>
-        </p>
-    )
+    if (props.textAlign) {
+        return (
+            <p className={`${style.subtitle} ${style.textAlign}`}>
+                {props.subtitle}
+                <span>{props.gradient}</span>
+            </p>
+        )
+    } else {
+        return (
+            <p className={style.subtitle}>
+                {props.subtitle}
+                <span>{props.gradient}</span>
+            </p>
+        )
+    }
+
 }
