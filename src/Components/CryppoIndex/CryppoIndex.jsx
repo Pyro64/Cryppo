4074fa58ed7542f5f6126bec04539cc5f990d361
kykakre@mail.ru <kykakre@mail.ru>
2022-03-24 17:58:33 +0400

    24 end

--- src/Components/CryppoIndex/CryppoIndex.jsx
+++ src/Components/CryppoIndex/CryppoIndex.jsx
@@ -7,8 +7,7 @@ import Btn from '../Btn/Btn'
 
 import Suggestions from '../Suggestions/Suggestions'
 import BenefitsIndex from '../BenefitsIndex/BenefitsIndex'
-import BenefitsIndexItem from '../BenefitsIndex/BenefitsIndexItem'
-
+import AboutIndex from '../AboutIndex/AboutIndex'
 
 export default function CryppoIndex(props) {
     return (
@@ -28,20 +27,20 @@ export default function CryppoIndex(props) {
             <div className="aboutIndex">
                 <div className="aboutIndex__block">
                     <Subtitle subtitle="О проекте" gradient="." />
-                    <Text text="Проект «CRYPPO INDEX» был создан в 2018 году, изначально для личного использования, как инструмент минимизации рисков и преумножения капитала на высоко волатильном рынке цифровых активов." />
+                    <Text modify={{marginBottom:"20px"}} text="Проект «CRYPPO INDEX» был создан в 2018 году, изначально для личного использования, как инструмент минимизации рисков и преумножения капитала на высоко волатильном рынке цифровых активов." />
                     <Text text="В течение первых 19 месяцев было осуществлено 1574 инвестиционных операции. Итоговая доходность за последние  12 месяцев составила 38% в долларовом эквиваленте (USDT). С июля 2020 года проект стал открытым для внешних инвесторов. Начальная стоимость индекса составляла $1. Стоимость индекса на текущий момент составляет $24.76." />
                 </div>
                 <div className={style.item}>
-
+                    <AboutIndex/>
                 </div>
             </div>
             <div className="benefistIndex">
-                <Subtitle  subtitle="Преимущества и гарантии" gradient="."/>
+                <Subtitle modify={{textAlign:"center"}} subtitle="Преимущества и гарантии" gradient="."/>
                 <BenefitsIndex benefits={props.benefits}/>
             </div>
-        </div>
-
-           
+            <Subtitle modify={{textAlign:"center"}} subtitle="О технологии" gradient="."/>
+            <Text modify={{textAlign:"center"}} text="Специально обученная нейронная сеть в режиме реального времени 24/7, анализирует состояние рынка, составляет прогноз, совершает от 48 до 96 операций ребалансирования в сутки и изменяет балансовый состав индекса для увеличения финансовой стоимости активов инвесторов."/>
+          </div>
         </div>
     )
 }
