5fdf34408d56f198b80bfb7bd93aadf1f71cb315
RomanCentr <trifonov.roman64@gmail.com>
2022-04-07 11:54:20 +0400

    footer refactor

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,55 +1,53 @@
-import React, { useEffect, useState} from "react";
+import React from "react";
 import Logo from "../Logo/Logo";
-import FooterMail from "../FooterMail/FooterMail";
-import FooterSocial from "../FooterSocial/FooterSocial";
-import FooterPhone from "../FooterPhone/FooterPhone";
+import FooterMail from "./FooterMail";
+import FooterSocial from "./FooterSocial";
+import FooterPhone from "./FooterPhone";
 import style from './Footer.module.scss'
 
 const Footer = (props) => {
-    let elementItem = props.mail.map(e =>
-        <FooterMail
-            id={e.id}
-            key={e.id}
-            text={e.text}
-        />
-    )
-    let elementIcon = props.icon.map(e =>
-      <FooterSocial
-          id={e.id}
-          key={e.id}
-          icon={e.icon}
-          href={e.href}
-      />
+  let elementItem = props.mail.map(e =>
+    <FooterMail
+      id={e.id}
+      key={e.id}
+      text={e.text}
+    />
+  )
+  let elementIcon = props.icon.map(e =>
+    <FooterSocial
+      id={e.id}
+      key={e.id}
+      icon={e.icon}
+      href={e.href}
+    />
   )
   let elementPhone = props.phone.map(e =>
     <FooterPhone
-        id={e.id}
-        key={e.id}
-        phone={e.phone}
+      id={e.id}
+      key={e.id}
+      phone={e.phone}
     />
-)
+  )
 
 
   return (
-      <div className={style.footer}>
-           <div className={style.footerItem}>
-              <div className={style.logo}>
-                <Logo img={props.logo} bussiness={props.bussiness} index={props.index} cryppo={props.cryppo} />
-                <div className={style.posothialWrapper}>
-                <a href="#" className={style.posothialText}>PRIVACY POLICY</a>
-                <a href="#" className={style.posothialText}>TERMS OF USE</a>
-                </div>
-
-              </div>
-
-              {elementItem} 
-              <div className={style.social}>
-                <div className={style.number}>Телефон для связи:</div>
-                <div className={style.phone}>{elementPhone}</div>
-                <div className={style.iconContainer}>{elementIcon}</div>
-              </div>
-              </div>
-</div>
+    <div className={style.container}>
+      <div className={style.flex}>
+        <div className={style.item}>
+          <Logo img={props.logo} bussiness={props.bussiness} index={props.index} cryppo={props.cryppo} />
+          <div className={style.wrapper}>
+            <a href="#">PRIVACY POLICY</a>
+            <a href="#">TERMS OF USE</a>
+          </div>
+        </div>
+        {elementItem}
+        <div className={style.social}>
+          <div className={style.number}>Телефон для связи:</div>
+          <div className={style.phone}>{elementPhone}</div>
+          <div className={style.iconContainer}>{elementIcon}</div>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Footer;
