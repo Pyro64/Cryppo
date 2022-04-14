b79d98d9ec8246e658cdc94cd2d5e7855395b946
kykakre <kykakre@mail.ru>
2022-04-14 10:44:15 +0400

    cryppo props

--- src/Components/Cryppo/Cryppo.jsx
+++ src/Components/Cryppo/Cryppo.jsx
@@ -1,26 +1,37 @@
 import React, { useState } from "react";
 import { Route, Routes } from "react-router";
 import FooterContainer from "../Footer/FooterContainer";
-import Header from '../Header/Header'
+import Header from "../Header/Header";
 import CryppoLanding from "./CryppoLanding/CryppoLanding";
 import CryppoLk from "../Lk/CryppoLk/CryppoLk";
 
 export default function Cryppo(props) {
-    return (
-        <div>
-            <Header logo={props.logo} btn={props.btn} login={props.login} isLogin={props.isLogin} user={props.user} />
-            <Routes>
-                <Route
-                    path="/"
-                    element={<CryppoLanding logo={props.logo} />}
-                />
-                <Route
-                    path="/lk/*"
-                    element={<CryppoLk card={props.card} />}
-                />
-            </Routes>
-            <FooterContainer logo={props.logo} />
-
-        </div>
-    )
+  return (
+    <div>
+      <Header
+        logo={props.logo}
+        btn={props.btn}
+        login={props.login}
+        isLogin={props.isLogin}
+        user={props.user}
+      />
+      <Routes>
+        <Route path="/" element={<CryppoLanding logo={props.logo} />} />
+        <Route
+          path="/lk/*"
+          element={
+            <CryppoLk
+              card={props.card}
+              bankCard={props.bankCard}
+              invest={props.invest}
+              newCard={props.newCard}
+              operationData={props.operationData}
+              statistic={props.statistic}
+            />
+          }
+        />
+      </Routes>
+      <FooterContainer logo={props.logo} />
+    </div>
+  );
 }
