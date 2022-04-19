6740085164283ca9e1b364629291c6eae20e351b
Pyro64 <trifonov.roman64@gmail.com>
2022-04-19 18:35:29 +0400

    oh vajno

--- src/Components/Lk/News/News.jsx
+++ src/Components/Lk/News/News.jsx
@@ -1,12 +1,52 @@
-import React from 'react'
+
+import React from 'react';
 import SubtitleLk from '../SubtitleLk/SubtitleLk'
+import { Autoplay, Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { useSwiper } from 'swiper/react';
+import NewsItem from './NewsItem'
+import 'swiper/scss';
 import style from "./News.module.scss";
-import NewsSlider from './NewsSlider';
-export default function News(props) {
+
+const News = (props) => {
+    const swiper = useSwiper();
+    console.log(swiper)
+    let newsItem = props.news.map(e =>
+        <SwiperSlide>
+
+            <NewsItem
+                id={e.id}
+                key={e.key}
+                text={e.text}
+                img={e.img}
+            />
+        </SwiperSlide>
+    )
     return (
         <div className={style.container}>
-            <SubtitleLk subtitle="Для вас" />
-            <NewsSlider news={props.news} />
+            <div className={style.block}>
+                <div className={style.flex}>
+                    <SubtitleLk subtitle="Для вас" />
+                    <div className={style.navigation}>
+                        <div className={style.prev}></div>
+                        <div className={style.next}></div>
+                    </div>
+                </div>
+                <Swiper
+                    modules={[Navigation, Pagination, Autoplay]}
+                    slidesPerView={4}
+                    loop={true}
+                    // autoplay={{ delay: 5000, }}
+                    navigation
+                    pagination
+                    className='news-slider'
+                >
+                    {newsItem}
+                </Swiper>
+            </div>
         </div>
-    )
+    );
 }
+
+export default News;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import store from './Redux/Store'
 import { Provider } from "react-redux";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
+
+ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>
+  , document.getElementById("root")
 );
