369830f678216b2ab30cac9441fc164e63f59a2d
Snail <Held2001@mail.ru>
2022-04-26 18:35:07 +0300

    Form login with validation

--- src/Components/Authorization/Authorization.jsx
+++ src/Components/Authorization/Authorization.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { Field, reduxForm } from "redux-form";
+import { email, required, password } from "../../Utils/validators";
+import Input from "../Input/Input";
 
 export default function Authorization(props) {
-    debugger;
-    function Login(){
+    const AuthorizationForm = (props) => {
+        return (
+            <form onSubmit={props.handleSubmit}>
+                <Field component={Input} name='email' validate={[required, email]} />
+                <Field component={Input} name='password' validate={[required, password]} />
+                <button className="btn">Авторизоваться</button>
+            </form>
+        )
+    }
+    const AuthorizationReduxForm = reduxForm({ form: 'authorization' })(AuthorizationForm);
+    const onSubmit = (formData) => {
+        console.log(formData)
         let password = document.querySelector("#password-input").value;
         let email = document.querySelector("#email-input").value;
         props.authorizationPostThunkCreator(email, password);
     }
+
     return (
-        <div>
-            <div>Authorization</div>
-            <input id='email-input'/>
-            <input id='password-input'/>
-            <button id='submit-button' onClick={Login}>Авторизоваться</button>
-        </div>
+        <AuthorizationReduxForm onSubmit={onSubmit}/>
     )
 }
---
+++ src/Utils/validators.js
@@ -0,0 +1,16 @@
+export const required = (value) => {
+    if (value) return undefined;
+    return 'required'
+}
+export const maxLenghtCreator = (maxLenght) => (value) => {
+    if (value.length > maxLenght) return `max lenght is ${maxLenght} symbols`;
+    return undefined
+}
+export const email = (value) => {
+    if (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) return 'Invalid email address';
+    return undefined
+}
+export const password = (value) => {
+    if (value && value.length < 7 ) return 'Small password';
+    return undefined
+}
