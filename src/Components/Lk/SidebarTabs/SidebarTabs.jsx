234a5bc870686dc8ee3869350c64ba8fdfdebae7
kykakre <kykakre@mail.ru>
2022-04-26 17:02:13 +0400

    modal final

--- src/Components/Lk/SidebarTabs/SidebarTabs.jsx
+++ src/Components/Lk/SidebarTabs/SidebarTabs.jsx
@@ -6,17 +6,22 @@ export default function SidebarTabs(props) {
   let cash = <div className={style.subtitle}>{props.tabs.cash}</div>;
   let statistic = <div className={style.subtitle}>{props.tabs.statistic}</div>;
   let text = <div className={style.text}>{props.tabs.text}</div>;
-  let povered = <img src={props.tabs.povered} className={style.poveredIcon} alt="img" />;
+  let povered = (
+    <img src={props.tabs.povered} className={style.poveredIcon} alt="img" />
+  );
   return (
     <div className={style.block}>
       <div className={style.wrapper}>
-        <img src={props.tabs.icon} className={style.icon} alt="img" />
-        <div className={style.textWrapper}>
-          <div>{props.tabs.title ? title : ""}</div>
-          <div>{props.tabs.statistic ? statistic : ""}</div>
-          <div>{props.tabs.cash ? cash : ""}</div>
-          <div>{props.tabs.text ? text : ""}</div>
+        <div className={style.info}>
+          <img src={props.tabs.icon} className={style.icon} alt="img" />
+          <div className={style.textWrapper}>
+            <div>{props.tabs.title ? title : ""}</div>
+            <div>{props.tabs.statistic ? statistic : ""}</div>
+            <div>{props.tabs.cash ? cash : ""}</div>
+            <div>{props.tabs.text ? text : ""}</div>
+          </div>
         </div>
+
         <div>{props.tabs.povered ? povered : ""}</div>
       </div>
     </div>
