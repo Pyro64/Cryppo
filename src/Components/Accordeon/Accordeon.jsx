6e0b0c71a4e524728e12a28b2d2a67a90c1166ed
RomanCentr <trifonov.roman64@gmail.com>
2022-04-26 17:07:51 +0400

    accordeon

--- src/Components/Accordeon/Accordeon.jsx
+++ src/Components/Accordeon/Accordeon.jsx
@@ -1,17 +1,32 @@
 import { React, useState } from "react";
+import { CSSTransition } from 'react-transition-group';
 import style from './Accordeon.module.scss'
 export default function Accordeon(props) {
     const [open, setOpen] = useState(false);
-    let toggleHandler = (e) => {
-        setOpen(!open);
-    };
+    const classNames = {
+        appear: `${style.hover}`,
+        appearActive: 'my-active-appear',
+        appearDone: 'my-done-appear',
+        enter: `${style.hoverEnter}`,
+        enterActive: 'my-active-enter',
+        enterDone: `${style.hoverEnterDone}`,
+        exit: `${style.hoverExit}`,
+        exitActive: 'my-active-exit',
+        exitDone: `${style.hoverExitDone}`,
+    }
     return (
-        <div className={open ? `${style.container} ${style.open}` : `${style.container}`}>
-            <div className={style.block} onClick={toggleHandler}>
-                <div>{props.title}</div>
-                <div className={style.dropdown}></div>
+        <CSSTransition
+            in={open}
+            classNames={{ ...classNames }}
+        >
+            <div className={open ? `${style.container} ${style.open}` : `${style.container}`}>
+                <div className={style.block} onClick={() => setOpen(!open)}>
+                    <div>{props.title}</div>
+                    <div className={style.dropdown}></div>
+                </div>
+                <div className={style.content}>{props.text} </div>
             </div>
-            <div className={style.content}>{props.text} </div>
-        </div>
+        </CSSTransition>
+
     )
 }
