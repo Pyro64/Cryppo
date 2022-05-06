8bec72ddfc5e5be85c8833d7ee07c0c4ede1b466
Snail <Held2001@mail.ru>
2022-05-06 14:04:06 +0300

    Edit Filter Operations

--- src/Components/Lk/Events/Event.jsx
+++ src/Components/Lk/Events/Event.jsx
@@ -1,11 +1,24 @@
-import React, {useRef} from "react";
-import  style from  './Event.module.scss'
+import React, { useRef, useEffect } from "react";
+import style from "./Event.module.scss";
 import SearchBar from "./SearchBar/SearchBar";
 import StatisticCash from "../StatisticCash/StatisticCash";
 import Operation from "../CryppoLk/CryppoLkComponents/OperationLk/Operation";
-
+import { useParams } from "react-router-dom";
 
 const Event = (props) => {
+    let operations = props.operationData.operation;
+    let currency = props.currency;
+    const chartRef = useRef(null);
+    const { category, subcategory } = useParams();
+    useEffect(() => {
+        if (props.filter === true) {
+            currency = props.filterOperationsThunkCreator(
+                props,
+                category,
+                subcategory
+            );
+        }
+    }, []);
 
     return (
         <div className="main container">
@@ -15,11 +28,28 @@ const Event = (props) => {
                 removeTag={props.removeTag}
                 inputChange={props.inputChange}
             />
-            <StatisticCash filter={false} isHover={props.isHover} setChartText={props.setChartText} currency={props.currency} chartTextData={props.chartTextData} initChartText={props.initChartText} operationData={props.operationData} setModal={props.setModal} operationModal={props.operationModal} isModal={props.isModal} switchModal={props.switchModal}/>
+            <StatisticCash
+                filter={false}
+                isHover={props.isHover}
+                setChartText={props.setChartText}
+                currency={props.currency}
+                chartTextData={props.chartTextData}
+                initChartText={props.initChartText}
+                operationData={props.operationData}
+                setModal={props.setModal}
+                operationModal={props.operationModal}
+                isModal={props.isModal}
+                switchModal={props.switchModal}
+            />
             <div className={style.wrapper}>
-                <Operation operationData={props.operationData} setModal={props.setModal} operationModal={props.operationModal} isModal={props.isModal} switchModal={props.switchModal}/>
+                <Operation
+                    operationData={props.operationData}
+                    setModal={props.setModal}
+                    operationModal={props.operationModal}
+                    isModal={props.isModal}
+                    switchModal={props.switchModal}
+                />
             </div>
-
         </div>
     );
 };
