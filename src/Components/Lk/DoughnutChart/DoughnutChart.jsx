d9107e59a52f3229402d9b623e6dd511e1c3b8ca
Pyro64 <trifonov.roman64@gmail.com>
2022-05-21 17:42:45 +0300

    statistic item

--- src/Components/Lk/DoughnutChart/DoughnutChart.jsx
+++ src/Components/Lk/DoughnutChart/DoughnutChart.jsx
@@ -3,83 +3,84 @@ import style from "./DoughnutChart.module.scss";
 import { Doughnut } from "react-chartjs-2";
 import ChartText from "../StatisticCash/ChartText";
 import StatisticCashItem from "../StatisticCash/StatisticCashItem";
-import {ArcElement, Chart as ChartJS, Legend, Tooltip} from "chart.js";
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js";
 ChartJS.register(ArcElement, Tooltip, Legend);
 const DoughnutChart = (props) => {
-    
-    let dataItems = [];
-    let backgroundColorItems = [];
-    props.operations.map((e) => {
-        dataItems.push(e.percent);
-        backgroundColorItems.push(e.color);
-    });
-    let elementItem = props.operations.map((e) => (
-        <StatisticCashItem
-            updateChart={props.updateChart}
-            child={e.childExpensesStatistics}
-            addTag={props.addTag}
-            setChartText={props.setChartText}
-            initChartText={props.initChartText}
-            percent={e.percent}
-            id={e.id}
-            key={e.id}
-            color={e.color}
-            category={e.category}
-            cash={e.cash}
-        />
-    ));
-    const options = {
-        plugins: {
-            tooltip: {
-                enabled: false,
-            },
+  let dataItems = [];
+  let backgroundColorItems = [];
+  props.operations.map((e) => {
+    dataItems.push(e.percent);
+    backgroundColorItems.push(e.color);
+  });
+  let elementItem = props.operations.map((e) => (
+    <StatisticCashItem
+      updateChart={props.updateChart}
+      child={e.childExpensesStatistics}
+      addTag={props.addTag}
+      setChartText={props.setChartText}
+      initChartText={props.initChartText}
+      percent={e.percent}
+      id={e.id}
+      key={e.id}
+      color={e.color}
+      category={e.category}
+      cash={e.cash}
+    />
+  ));
+  const options = {
+    plugins: {
+      tooltip: {
+        enabled: false,
+      },
+    },
+    layout: {
+      padding: {
+        bottom(ctx) {
+          const chart = ctx.chart;
+          let pb = 0;
+          chart.data.datasets.forEach(function(el) {
+            const hOffset = el.hoverOffset || 0;
+            pb = Math.max(hOffset / 2 + 5, pb);
+          });
+          return pb;
         },
-        layout: {
-            padding: {
-                bottom(ctx) {
-                    const chart = ctx.chart;
-                    let pb = 0;
-                    chart.data.datasets.forEach(function (el) {
-                        const hOffset = el.hoverOffset || 0;
-                        pb = Math.max(hOffset / 2 + 5, pb);
-                    });
-                    return pb;
-                },
-            },
-        },
-        onHover: function(evt, element) {
-            if(element.length > 0) {
-                console.log(element,element[0]._datasetIndex)
-                // you can also get dataset of your selected element
-                console.log(data.datasets[element[0]._datasetIndex])
-            }
-        },
+      },
+    },
+    onHover: function(evt, element) {
+      if (element.length > 0) {
+        element[0].active = true;
+        console.log(element, element[0]._datasetIndex);
 
-        cutout: 110,
-    };
-    const data = {
-        labels: [],
-        datasets: [
-            {
-                data: dataItems,
-                backgroundColor: backgroundColorItems,
-                borderWidth: 0,
-                hoverOffset: 20,
-            },
-        ],
-    };
-    return (
-        <div className={style.flex}>
-            <div className={style.items}>{elementItem}</div>
-            <div className={style.chartInner}>
-                <Doughnut data={data} options={options} />
-                <ChartText
-                    isHover={props.isHover}
-                    chartTextData={props.chartTextData}
-                />
-            </div>
-        </div>
-    );
+        // you can also get dataset of your selected element
+        console.log(data);
+      }
+    },
+
+    cutout: 110,
+  };
+  const data = {
+    labels: [],
+    datasets: [
+      {
+        data: dataItems,
+        backgroundColor: backgroundColorItems,
+        borderWidth: 0,
+        hoverOffset: 20,
+      },
+    ],
+  };
+  return (
+    <div className={style.flex}>
+      <div className={style.items}>{elementItem}</div>
+      <div className={style.chartInner}>
+        <Doughnut data={data} options={options} />
+        <ChartText
+          isHover={props.isHover}
+          chartTextData={props.chartTextData}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default DoughnutChart;
--- src/Components/Lk/StatisticCash/StatisticCashItem.jsx
+++ src/Components/Lk/StatisticCash/StatisticCashItem.jsx
@@ -1,39 +1,44 @@
 import React from "react";
 import style from "./StatisticCash.module.scss";
 import { NavLink, useParams } from "react-router-dom";
-
+import { CloseOutlined } from "@ant-design/icons";
 const StatisticCashItem = (props) => {
-    let child = props.child
-    let value = props;
-    const tag = props.category;
-    const { category, subcategory } = useParams();
-    let url = `/cryppo/lk/event/${props.category}`;
-    if (category !== undefined) {
-        url = `/cryppo/lk/event/${category}/${props.category}`;
+  let child = props.child;
+  let value = props;
+  const tag = props.category;
+  const { category, subcategory } = useParams();
+  let url = `/cryppo/lk/event/${props.category}`;
+  if (category !== undefined) {
+    url = `/cryppo/lk/event/${category}/${props.category}`;
+  }
+  const foo = () => {
+    if (child !== undefined) {
+      props.updateChart(child);
     }
-    const foo = () => {
-        if (child !== undefined){
-            props.updateChart(child)
-        }
-    }
-    return (
-        <NavLink
-            to={url}
-            state={{ name: props.category }}
-            className={style.item}
-            onMouseEnter={() => props.setChartText(value)}
-            onMouseLeave={() => props.initChartText()}
-            onClick={foo}
-        >
-            <div
-                style={{ background: `${props.color}` }}
-                className={style.circle}
-            ></div>
-            <div className={style.category}>{props.category}</div>
-            <div className={style.category}>{props.cash}</div>
-            <div className={style.category}>{props.currency}</div>
-        </NavLink>
-    );
+  };
+  return (
+    <div className={style.wrapper}>
+      <NavLink
+        to={url}
+        state={{ name: props.category }}
+        className={style.item}
+        onMouseEnter={() => props.setChartText(value)}
+        onMouseLeave={() => props.initChartText()}
+        onClick={foo}
+      >
+        <div
+          style={{ background: `${props.color}` }}
+          className={style.circle}
+        ></div>
+        <div className={style.category}>{props.category}</div>
+        <div className={style.category}>{props.cash}</div>
+        <div className={style.category}>{props.currency}</div>
+      </NavLink>
+      <div className={style.cross}>
+        <CloseOutlined twoToneColor="#eb2f96" />
+      </div>
+    </div>
+  );
 };
 
 export default StatisticCashItem;
