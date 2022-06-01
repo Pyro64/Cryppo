7a4bbff67634032d4ea975a953ee8b58b2d028a0
kykakre <kykakre@mail.ru>
2022-06-01 18:12:20 +0400

    registry

--- src/Components/UI/LkInput/LkInput.jsx
+++ src/Components/UI/LkInput/LkInput.jsx
@@ -1,11 +1,11 @@
 import { Input } from "antd";
 import React from "react";
 import style from "./LkInput.module.scss";
-const LkInput = ({ title, ...props }) => {
+const LkInput = ({ placeholder, title, ...props }) => {
   return (
     <div className={style.container}>
       <p>{title}</p>
-      <Input {...props} />
+      <Input {...props} placeholder={placeholder} className={style.input} />
     </div>
   );
 };
