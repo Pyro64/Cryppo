ec8d400ac8854f800e02a2e700f22d6e40c75c2c
Pyro64 <trifonov.roman64@gmail.com>
2022-06-08 12:16:33 +0400

    18 react v.2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import store from './Redux/Store'
+import React from "react";
+import App from "./App";
+import store from "./Redux/Store";
 import { Provider } from "react-redux";
-
-
-ReactDOM.render(
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
   <Provider store={store}>
     <App />
   </Provider>
-  , document.getElementById("root")
 );
