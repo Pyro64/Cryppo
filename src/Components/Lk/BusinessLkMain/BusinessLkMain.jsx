2f161cb168e305758fad17280be3dd2e9fdb8576
kykakre <kykakre@mail.ru>
2022-06-21 16:20:58 +0400

    chart

--- src/Components/Lk/BusinessLkMain/BusinessLkMain.jsx
+++ src/Components/Lk/BusinessLkMain/BusinessLkMain.jsx
@@ -7,55 +7,55 @@ import DoughnutChart from "../DoughnutChart/DoughnutChart";
 import CompositionIndex from "../CompositionIndex/CompositionIndex";
 import SidebarTabs from "../SidebarTabs/SidebarTabs";
 const BusinessLkMain = (props) => {
-    return (
-        <div className="main container">
-            <div className="flex ">
-                <div className="sidebar">
-                    <div className="sidebar__inner">
-                        <SubtitleLk arrow={false} subtitle="Счета" />
-                        <Check
-                            isBankCard={false}
-                            cardList={props.cardList}
-                            currencyList={props.currencyList}
-                        />
-                        <SidebarTabs tabs={props.tabs.addTerminal} />
-                    </div>
-                </div>
-                <div className="content">
-                    <SubtitleLk subtitle="Главная" />
-                    <div className="block">
-                        <DoughnutChart
-                            updateChart={props.updateChart}
-                            filter={true}
-                            isHover={props.isHover}
-                            setChartText={props.setChartText}
-                            operations={props.operationList}
-                            chartTextData={props.chartTextData}
-                            initChartText={props.initChartText}
-                            disableItem={props.disableItem}
-                        />
-                    </div>
-                    <SubtitleLk arrow={true} subtitle="Статистика валют" />
-                    <div className="block">
-                        <MyBar
-                            compositions={props.barData.compositions}
-                            height={`300px`}
-                            changeActiveIndex={props.changeActiveIndex}
-                        />
-                    </div>
-
-                    <SubtitleLk arrow={true} subtitle="Последние операции" />
-                    <Operation
-                        operationList={props.operationList}
-                        setModal={props.setModal}
-                        operationModal={props.operationModal}
-                        isModal={props.isModal}
-                        switchModal={props.switchModal}
-                    />
-                </div>
-            </div>
+  return (
+    <div className="main container">
+      <div className="flex ">
+        <div className="sidebar">
+          <div className="sidebar__inner">
+            <SubtitleLk arrow={false} subtitle="Счета" />
+            <Check
+              isBankCard={false}
+              cardList={props.cardList}
+              currencyList={props.currencyList}
+            />
+            <SidebarTabs tabs={props.tabs.addTerminal} />
+          </div>
         </div>
-    );
+        <div className="content">
+          <SubtitleLk subtitle="Главная" />
+          <div className="block">
+            <DoughnutChart
+              updateChart={props.updateChart}
+              filter={true}
+              isHover={props.isHover}
+              setChartText={props.setChartText}
+              operations={props.expenses}
+              chartTextData={props.chartTextData}
+              initChartText={props.initChartText}
+              disableItem={props.disableItem}
+            />
+          </div>
+          <SubtitleLk arrow={true} subtitle="Статистика валют" />
+          <div className="block">
+            <MyBar
+              compositions={props.barData.compositions}
+              height={`300px`}
+              changeActiveIndex={props.changeActiveIndex}
+            />
+          </div>
+
+          <SubtitleLk arrow={true} subtitle="Последние операции" />
+          <Operation
+            operationList={props.operationList}
+            setModal={props.setModal}
+            operationModal={props.operationModal}
+            isModal={props.isModal}
+            switchModal={props.switchModal}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BusinessLkMain;
