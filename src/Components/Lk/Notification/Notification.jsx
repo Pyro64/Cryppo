27087d846e59f93031464e0bc2f99f169ba76907
kykakre <kykakre@mail.ru>
2022-06-22 11:22:40 +0400

    fix notificathion

--- src/Components/Lk/Notification/Notification.jsx
+++ src/Components/Lk/Notification/Notification.jsx
@@ -4,67 +4,69 @@ import style from "./Notification.module.scss";
 
 import SubtitleLk from "../../UI/SubtitleLk/SubtitleLk";
 export default function Notification(props) {
-    let notification = [
-        <NotifocathionItem
-            id={useId}
-            email={props.notificationsSettings.systemEmail}
-            telegram={props.notificationsSettings.systemTg}
-            text="Системные уведомления"
-        />,
-        <NotifocathionItem
-            id={useId}
-            key={useId}
-            email={props.notificationsSettings.entryEmail}
-            telegram={props.notificationsSettings.entryTg}
-            text="Успешный вход"
-        />,
-        <NotifocathionItem
-            id={useId}
-            key={useId}
-            email={props.notificationsSettings.failEntryEmail}
-            telegram={props.notificationsSettings.failEntryTg}
-            text="Неуспешный вход"
-        />,
-        <NotifocathionItem
-            id={useId}
-            key={useId}
-            email={props.notificationsSettings.paymentGetEmail}
-            telegram={props.notificationsSettings.paymentGetTg}
-            text="Получен платеж"
-        />,
-        <NotifocathionItem
-            id={useId}
-            key={useId}
-            email={props.notificationsSettings.paymentConfirmedEmail}
-            telegram={props.notificationsSettings.paymentConfirmedTg}
-            text="Отправлен платеж"
-        />,
-        <NotifocathionItem
-            id={useId}
-            key={useId}
-            email={props.notificationsSettings.passwordChangeEmail}
-            telegram={props.notificationsSettings.passwordChangeTg}
-            text="Изменен пароль"
-        />,
-        <NotifocathionItem
-            id={useId}
-            key={useId}
-            email={props.notificationsSettings.notificationsChangeEmail}
-            telegram={props.notificationsSettings.notificationsChangeTg}
-            text="Изменены уведомления"
-        />,
-    ];
+  let notification = [
+    <NotifocathionItem
+      id={useId}
+      email={props.notificationsSettings.systemEmail}
+      telegram={props.notificationsSettings.systemTg}
+      text="Системные уведомления"
+    />,
+    <NotifocathionItem
+      id={useId}
+      key={useId}
+      email={props.notificationsSettings.entryEmail}
+      telegram={props.notificationsSettings.entryTg}
+      text="Успешный вход"
+    />,
+    <NotifocathionItem
+      id={useId}
+      key={useId}
+      email={props.notificationsSettings.failEntryEmail}
+      telegram={props.notificationsSettings.failEntryTg}
+      text="Неуспешный вход"
+    />,
+    <NotifocathionItem
+      id={useId}
+      key={useId}
+      email={props.notificationsSettings.paymentGetEmail}
+      telegram={props.notificationsSettings.paymentGetTg}
+      text="Получен платеж"
+    />,
+    <NotifocathionItem
+      id={useId}
+      key={useId}
+      email={props.notificationsSettings.paymentConfirmedEmail}
+      telegram={props.notificationsSettings.paymentConfirmedTg}
+      text="Отправлен платеж"
+    />,
+    <NotifocathionItem
+      id={useId}
+      key={useId}
+      email={props.notificationsSettings.passwordChangeEmail}
+      telegram={props.notificationsSettings.passwordChangeTg}
+      text="Изменен пароль"
+    />,
+    <NotifocathionItem
+      id={useId}
+      key={useId}
+      email={props.notificationsSettings.notificationsChangeEmail}
+      telegram={props.notificationsSettings.notificationsChangeTg}
+      text="Изменены уведомления"
+    />,
+  ];
 
-    return (
-        <div className={style.content}>
-            <div className={style.title}>Мгновенные уведомления</div>
-            <div className={style.text}>
-                Выберите, какие уведомления вы хотите получать и куда (Email,
-                Telegram, Телефон). Каждый раз, когда произойдет выбранное
-                событие, вам будет отправлено сообщение.
-            </div>
-            {notification}
-            <button onClick></button>
+  return (
+    <div className="main container">
+      <div className={style.content}>
+        <div className={style.title}>Мгновенные уведомления</div>
+        <div className={style.text}>
+          Выберите, какие уведомления вы хотите получать и куда (Email,
+          Telegram, Телефон). Каждый раз, когда произойдет выбранное событие,
+          вам будет отправлено сообщение.
         </div>
-    );
+        {notification}
+        <button onClick></button>
+      </div>
+    </div>
+  );
 }
