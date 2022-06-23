9fcd6e880a5b836474fbdad6a32fdea91a1e755f
kykakre <kykakre@mail.ru>
2022-06-23 14:16:50 +0400

    page in work

--- src/Components/Pages/CryppoLk.jsx
+++ src/Components/Pages/CryppoLk.jsx
@@ -5,32 +5,31 @@ import CryppoLkSettings from "../Lk/CryppoLkSettings/CryppoLkSettings";
 import CryppoLkTranslationContainer from "../Lk/CryppoLkTranslation/CryppoLkTranslationContainer";
 import CryppoLkInvestContainer from "../Lk/CryppoLkInvest/CryppoLkInvestContainer";
 import EventContainer from "../Lk/Events/EventContainer";
-
+import PageWork from "../Lk/PageWork/PageWork";
 export default function CryppoLk(props) {
-    // const get = () => props.getCryppoLkThunkCreator();
-    // useEffect(() => {
-    //   const interval = setInterval(get, 5000);
-    // }, []);
-    // alert(props.isLogin)
-    if (props.isLogin === false) return <Navigate to={"/"} />;
-    return (
-        <Routes>
-            <Route path="/" element={<CryppoLkMainContainer />} />
-            <Route
-                path="translations/*"
-                element={<CryppoLkTranslationContainer />}
-            />
-            <Route path="invest/*" element={<CryppoLkInvestContainer />} />
-            <Route path="settings/*" element={<CryppoLkSettings />} />
-            <Route path="event/*" element={<EventContainer filter={false} />} />
-            <Route
-                path="event/:category"
-                element={<EventContainer filter={true} />}
-            />
-            <Route
-                path="event/:category/:subcategory"
-                element={<EventContainer filter={true} />}
-            />
-        </Routes>
-    );
+  // const get = () => props.getCryppoLkThunkCreator();
+  // useEffect(() => {
+  //   const interval = setInterval(get, 5000);
+  // }, []);
+  // alert(props.isLogin)
+  if (props.isLogin === false) return <Navigate to={"/"} />;
+  return (
+    <Routes>
+      <Route path="/" element={<CryppoLkMainContainer />} />
+      <Route path="history" element={<PageWork />} />
+      <Route path="translations/*" element={<CryppoLkTranslationContainer />} />
+      <Route path="pageWork" element={<PageWork />} />
+      <Route path="invest/*" element={<CryppoLkInvestContainer />} />
+      <Route path="settings/*" element={<CryppoLkSettings />} />
+      <Route path="event/*" element={<EventContainer filter={false} />} />
+      <Route
+        path="event/:category"
+        element={<EventContainer filter={true} />}
+      />
+      <Route
+        path="event/:category/:subcategory"
+        element={<EventContainer filter={true} />}
+      />
+    </Routes>
+  );
 }
